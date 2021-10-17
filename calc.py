e8611b25ee653a49a16b494131be5831fe9ee1bd
Edbert Oey <edbertoey@gmail.com>
2021-10-17 15:25:05 +0000

    Complete Making a CLI Exercises

--- calc.py
+++ calc.py
@@ -1,16 +1,30 @@
 # calc.py
 
 import argparse
+# Import package to handle decimal precision of division subcommand set precision to 4 decimal places
+from decimal import *
+getcontext().prec = 4
 
 parser = argparse.ArgumentParser(description="CLI Calculator")
 
 subparsers = parser.add_subparsers(help="sub-command help", dest="command")
 
+# Exercse 1: Add any number of args to add command 
+# Docs https://docs.python.org/3/library/argparse.html#nargs
 add = subparsers.add_parser("add", help = "add integers")
-add.add_argument("ints_to_sum", nargs=2, type=int)
+add.add_argument("ints_to_sum", nargs='*', type=int)
 
-sub = subparsers.add_parser("sub", help = "subtract integers")
+sub = subparsers.add_parser("sub", help = "subtract two integers")
 sub.add_argument("ints_to_sub", nargs=2, type=int)
+
+# Exercise 2A: Add subcommand for multiplication
+multiply = subparsers.add_parser("multiply", help = "multiply two integers")
+multiply.add_argument("ints_to_multiply", nargs=2, type=int)
+
+# Exercise 2B: Add subcommand for division
+divide = subparsers.add_parser("divide", help = "divide two integers")
+divide.add_argument("ints_to_divide", nargs=2, type=int)
+
 
 args = parser.parse_args()
 
@@ -21,6 +35,18 @@ if args.command == "add":
 if args.command == "sub":
     our_sub = args.ints_to_sub[0] - args.ints_to_sub[1]
     print(f"The subtracted result of values is: {our_sub}")
+
+if args.command == "multiply":
+    our_multiply = args.ints_to_multiply[0] * args.ints_to_multiply[1]
+    print(f"The product of values is: {our_multiply}")
+
+if args.command == "divide":
+    try:
+        our_divide = Decimal(args.ints_to_divide[0] / args.ints_to_divide[1])
+        print(f"The division result of values is: {our_divide}")
+    except ZeroDivisionError:
+        print("Division by Zero - retry with different arguments")
+    
 
 # parser.add_argument("ints_to_sum", nargs=2, type=int)
 
